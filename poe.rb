0ec8a246a9654f0e79d8e1a245238df8fd533159
Brian Schlenker <bschlenk@umich.edu>
2015-02-07 12:22:45 -0800

    added default methods and close on escape

--- poe.rb
+++ poe.rb
@@ -1,9 +1,20 @@
+#!/usr/bin/env ruby
 require "gosu"
 
 class Poe < Gosu::Window
     def initialize width=800, height=600, fullscreen=false
         super
         self.caption = 'Poe Game'
+    end
+
+    def button_down id
+        close if id == Gosu::KbEscape
+    end
+
+    def update
+    end
+
+    def draw
     end
 end
 
